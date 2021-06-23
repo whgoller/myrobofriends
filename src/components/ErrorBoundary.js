b92c3dea9b335a962158e7946253654ccd4ec0cc
Wendy Goller <whgoller@gmail.com>
2021-06-23 13:11:18 -0500

    update to always return children if we get to the line

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -15,9 +15,9 @@ class ErrorBoundary extends Component {
     render() {
         if (this.state.hasError) {
             return <h1> Oooops. That is not good </h1>
-        } else {
-            return this.props.children;
         }
+
+        return this.props.children;
     }
 }
 export default ErrorBoundary;
