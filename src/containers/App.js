7552452b2f9f522d9335c6ac761572588abd88c1
Wendy Goller <whgoller@gmail.com>
2021-06-26 12:29:02 -0500

    update app.js to be a function that uses hooks for state instead of lifecycles.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,44 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardArray from '../components/CardArray';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import ErrorBoundary from "../components/ErrorBoundary";
 import '../containers/App.css'
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ""
-        }
-    }
-    componentDidMount() {
+function App() {
+
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchField] = useState("");
+
+    // this will run every time the app renders 
+    // this is a special function that is run automatically (it's part of the react lifecycle)
+    // we pass the optional array because we are setting a state in the function.  we give it an empty array which is a shortcut for the componentDidMount function. 
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => setRobots(users));
+    }, []);
+
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
 
-    }
-
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        });
-        //if empty robots.length will evaluate to 0 and 0 evaluates to false in JS and then turning the if into ternary opperator syntax
-        return !robots.length ? <h1>Loading</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardArray robots={filteredRobots} />
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            );
-    }
+    //if empty robots.length will evaluate to 0 and 0 evaluates to false in JS and then turning the if into ternary opperator syntax
+    return !robots.length ? <h1>Loading</h1> :
+        (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardArray robots={filteredRobots} />
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+        );
 }
 export default App;
