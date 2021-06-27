2eb3e16b7752bce0a6a30ff3ca574363ca941ca1
Wendy Goller <whgoller@gmail.com>
2021-06-27 10:40:15 -0500

    Add button for demonstration of hooks and react lifecyle

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,16 +7,19 @@ import '../containers/App.css'
 
 function App() {
 
+    // state 
     const [robots, setRobots] = useState([]);
     const [searchfield, setSearchField] = useState("");
+    const [count, setCount] = useState(0);
 
-    // this will run every time the app renders 
-    // this is a special function that is run automatically (it's part of the react lifecycle)
+    // useEffect will run every time the app renders, but will only run after the first render because of the second parameter of an empty [].
+    // useEffect is a special function that is run automatically (it's part of the react lifecycle)
     // we pass the optional array because we are setting a state in the function.  we give it an empty array which is a shortcut for the componentDidMount function. 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => setRobots(users));
+        console.log(count);
     }, []);
 
     const onSearchChange = (event) => {
@@ -32,6 +35,7 @@ function App() {
         (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
+                <button onClick={() => setCount(count + 1)}>Click Me!</button>
                 <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundary>
