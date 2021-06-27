8c20c5fd1a99986ba1f98bddafcb6cfc523bf299
Wendy Goller <whgoller@gmail.com>
2021-06-27 13:25:45 -0600

    add in connect the redux-react function that allows the use of the store for searchfield instead of the this.state object.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardArray from '../components/CardArray';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import ErrorBoundary from "../components/ErrorBoundary";
-import '../containers/App.css'
+import '../containers/App.css';
+
+import { setSearchField } from '../actions.js'
+
+// tell me what pieces of state I need to listen too and send down as props
+const mapStateToProps = state => {
+    return {
+        // searchfield: state.searchRobots.searchfield
+        searchField: state.searchField
+    }
+}
+
+// in the flux pattern dispatch is what sends action
+// tell me what props I need to listen to that are actions that need to get dispatched
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: [],
-            searchfield: ""
+            robots: []
         }
     }
     componentDidMount() {
@@ -19,22 +37,19 @@ class App extends Component {
             .then(users => this.setState({ robots: users }));
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-
-    }
 
     render() {
-        const { robots, searchfield } = this.state;
+        const { robots } = this.state;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
         //if empty robots.length will evaluate to 0 and 0 evaluates to false in JS and then turning the if into ternary opperator syntax
         return !robots.length ? <h1>Loading</h1> :
             (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundary>
                             <CardArray robots={filteredRobots} />
@@ -44,4 +59,8 @@ class App extends Component {
             );
     }
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// connect is a higher order function - it returns another function
+// connect accepts 2 parameters - mapStateToProps, mapDispatchToProps'
+// connect is what tells the component, in this case the App component, to subscibe to any state changes.
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
@@ -12,7 +12,11 @@ import 'tachyons';
 
 const store = createStore(searchRobots)
 
-// we don't really want to send the store object down to all the smaller components, so we will wrap our <App /> componenet in a Provider componenet and pass the store to the provider componenet. the Provider componenet will take care of passing down the store to all the other components down the component tree from the app.
+//Provider - we don't really want to send the store object down to all the smaller components, so we will wrap our <App /> componenet in a Provider componenet and pass the store to the provider componenet. the Provider componenet will take care of passing down the store to all the other components down the component tree from the app.
+// connect - optimized to make it so we don't have to use 
+//        store.subscribe 
+// 
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
