46f63b8a1e9464b1660bc69c582ccae74532e92c
Wendy Goller <whgoller@gmail.com>
2021-06-27 13:33:12 -0600

    adding notes

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,18 @@
 import { CHANGE_SEARCH_FIELD } from "./constants.js";
 
-export const setSearchField = (text) => ({
-    type: CHANGE_SEARCH_FIELD,
-    payload: text
-})
+export const setSearchField = (text) => {
+    return ({
+        type: CHANGE_SEARCH_FIELD,
+        payload: text
+    })
+}
 
-// type is the action being taken... this can be a string or a constant.
+// could also be written as the following:
+// export const setSearchField = (text) => ({
+//     type: CHANGE_SEARCH_FIELD,
+//     payload: text
+// });
+
+// type is the action being taken... this can be a string or a constant.npm start
 // it's capitalized because it's a constant 
 // payload is the data being entered
